d935d65ac5d28496c5dd20359c59331daf5fa76d
molingVae <1542664947@qq.com>
2019-07-25 16:36:36 +0800

    中药房前后端联调end

--- src/main/java/com/mshis/medicalSystem/dao/pharmacy/Uh03CompanyListDao.java
+++ src/main/java/com/mshis/medicalSystem/dao/pharmacy/Uh03CompanyListDao.java
@@ -1,10 +1,15 @@
 package com.mshis.medicalSystem.dao.pharmacy;
 
 import com.mshis.medicalSystem.pojo.bean.Uh03CompanyList;
+import io.swagger.models.auth.In;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * Created by 小白的PC on 2019/7/18
  */
 public interface Uh03CompanyListDao extends JpaRepository<Uh03CompanyList,Integer> {
+
+    Page<Uh03CompanyList> findAllByComNameContaining(String name, Pageable pageable);
 }
--- src/main/java/com/mshis/medicalSystem/dao/pharmacy/Uh03PriceDao.java
+++ src/main/java/com/mshis/medicalSystem/dao/pharmacy/Uh03PriceDao.java
@@ -1,7 +1,11 @@
 package com.mshis.medicalSystem.dao.pharmacy;
 
 import com.mshis.medicalSystem.pojo.bean.Uh03Price;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface Uh03PriceDao extends JpaRepository<Uh03Price,Integer> {
+
+    Page<Uh03Price> findAllByMediNameContaining(String name,Pageable pageable);
 }
